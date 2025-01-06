6014797a0b5aeab6b76986d72c4d2676be673872
antazoey <antyzoa@gmail.com>
2025-01-05 19:11:59 -0600

    perf: profile

--- scripts/perf.py
+++ scripts/perf.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import cProfile
+import io
+import pstats
 import sys
 from time import perf_counter
 
@@ -9,11 +12,18 @@ from click import command, echo, option
 
 @command(cls=ConnectedProviderCommand)
 @option("--transaction-iterations", help="The number of transactions to make", default=500)
-def cli(transaction_iterations):
+@option(
+    "--profile", is_flag=True, callback=lambda ctx, param, val: cProfile.Profile() if val else None
+)
+def cli(transaction_iterations, profile):
     """
     Performance check, useful for comparisons.
     """
     echo(f"Performance check: {chain.provider.name}")
+
+    if profile:
+        profile.enable()
+
     start = perf_counter()
     owner = accounts.test_accounts[0]
     contract = project.VyperContract.deploy(123, sender=owner)
@@ -21,6 +31,13 @@ def cli(transaction_iterations):
     for idx in range(transaction_iterations):
         number = contract.myNumber()
         contract.setNumber(number + 1, sender=owner)
+
+    if profile:
+        profile.disable()
+        s = io.StringIO()
+        ps = pstats.Stats(profile, stream=s).sort_stats("time")
+        ps.print_stats()
+        echo(s.getvalue().rstrip())
 
     finish = perf_counter()
     elapsed = finish - start
